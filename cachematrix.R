f0449c40813e0570fa05258e4f9cc3e9543c7912
Lori Testa <loriktesta@gmail.com>
2016-10-12 19:12:39 -0400

    R Programming Week 3 assignment submission
    Coursea.org course 2 (week 3) of the Data Science specialization

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This function is an example of lexical scoping and object
+## oriented programming in R. 
 
-## Write a short comment describing this function
+
+## Providing makeCacheMatrix with an inversible 
+## square matrix will return an object that has functions
+## that can be called against/with the object.
+## Returns a list of objects (functions)
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y = matrix()) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setsolve <- function(solve) m <<- solve
+        getsolve <- function() m
+        list(set = set, get = get,
+             setsolve = setsolve,
+             getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## Calling cacheSolve with this output from makeCacheMatrix will provide
+## you with the inverse matrix either computed or from cache
+## Returns an inverse matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$getsolve()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setsolve(m)
+        m
 }
+                
